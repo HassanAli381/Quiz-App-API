2576864d0ab62eab631747d7e7f388882366b02e
HassanAli381 <alhassanali599@gmail.com>
2025-10-19 10:50:22 +0300

    Refactor user router to remove asyncHandler and correctly import getUserExams

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const asyncHandler = require("express-async-handler"); // Import the handler
 
 const router = express.Router();
 const idParamHandler = require("../middlewares/id.middleware");
 
-const { getUserbyId } = require("../controllers/auth.controller")
+const { getUserbyId, getUserExams } = require("../controllers/user.controller")
 router.param("id", idParamHandler);
-router.get("/:id", asyncHandler(getUserbyId));
-router.get("/exams/:userId", asyncHandler(getUserbyId));
+router.get("/:id", getUserbyId);
+router.get("/exams/:userId", getUserExams);
 
 module.exports = router;
